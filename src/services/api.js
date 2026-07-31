3312aa2f5aae1b852dddde69434a8f878df26359
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-07-31 11:10:29 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,8 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://127.0.0.1:8000",
-   baseURL: "https://hcp-crm-project-b-17.onrender.com",
+   // baseURL: "https://hcp-crm-project-b-17.onrender.com",
+    baseURL: "https://hcp-crm-project-b-17.onrender.com",
 
   headers: {
     "Content-Type": "application/json",
